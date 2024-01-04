a0c9af6f3e797cb36eea243931d4cb216c6ca25c
Bantamlak <bantamlak29@gmail.com>
2024-01-04 08:50:35 +0300

    n is always positive

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,16 +4,16 @@
 
 def pascal_triangle(n):
     """Solves a pascal's triangle"""
-    if n <= 0:
+    if n > 0:
+        triangle = [[1]]
+
+        for i in range(n - 1):
+            temp = [0] + triangle[-1] + [0]
+            row = []
+            for j in range(len(temp) - 1):
+                row.append(temp[j] + temp[j + 1])
+            triangle.append(row)
+        return triangle
+    else:
         triangle = []
         return triangle
-
-    triangle = [[1]]
-
-    for i in range(n - 1):
-        temp = [0] + triangle[-1] + [0]
-        row = []
-        for j in range(len(temp) - 1):
-            row.append(temp[j] + temp[j + 1])
-        triangle.append(row)
-    return triangle
