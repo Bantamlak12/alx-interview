a3ca97c1808ef9bb3ce4c4bbed09c4603ff388c7
Bantamlak <bantamlak29@gmail.com>
2024-01-13 12:12:08 +0300

    solution added

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+File Name: 0-lockboxes.py
+
+Function:
+--------
+canUnlockAll(boxes)
+A module that determines if all the boxes can be opened.
+
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    parameter:
+    - boxes: A List of boxes that containes keys.
+
+    Returns:
+    - True if all boxes can be opened, else return False.
+    """
+    opened_boxes = [boxes[0]]
+    used_keys = []
+
+    for box in opened_boxes:
+        try:
+            for key in box:
+                if key not in used_keys:
+                    if key != 0:
+                        opened_boxes.append(boxes[key])
+                    used_keys.append(key)
+        except IndexError:
+            continue
+    if len(opened_boxes) == len(boxes):
+        return True
+    else:
+        return False
