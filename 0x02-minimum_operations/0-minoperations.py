9c91b3e8c451f3641e4c6597e8250dd7965f2243
Bantamlak <bantamlak29@gmail.com>
2024-01-16 11:15:29 +0300

    edge cases considered

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -16,6 +16,9 @@ def minOperations(n):
     """
     min_operation = 0
 
+    if n <= 0 or n == 1:
+        return 0
+
     for i in range(2, n + 1):
         while (n % i == 0):
             min_operation += i
