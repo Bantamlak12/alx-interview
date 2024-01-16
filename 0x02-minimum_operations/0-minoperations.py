a166f882398fe40c9c50f9cdabd64d6875e6d27f
Bantamlak <bantamlak29@gmail.com>
2024-01-16 10:48:01 +0300

    minimum operation tackled

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+File: 0-minoperations.py
+
+Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+    parameters:
+    n (int): Number of characters exactly to print
+
+    Returns:
+    - The minimum number of operations to result exactly `nH`
+    """
+    min_operation = 0
+
+    for i in range(2, n + 1):
+        while (n % i == 0):
+            min_operation += i
+            n /= i
+    return min_operation
