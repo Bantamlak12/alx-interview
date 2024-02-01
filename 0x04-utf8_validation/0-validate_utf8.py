5d95f1404c72d09a2fe73ab19d094a97c968a1bf
Bantamlak <bantamlak29@gmail.com>
2024-02-01 18:45:32 +0300

    handling the 8 least significant bits

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,20 +13,22 @@ def validUTF8(data: List[int]) -> bool:
     """
     i = 0
     while i < len(data):
-        if data[i] >> 7 == 0:
+        byte = data[i] & 0b11111111
+
+        if byte >> 7 == 0:
             i += 1
-        elif data[i] >> 5 == 0b110:
+        elif byte >> 5 == 0b110:
             if i + 1 < len(data) and data[i + 1] >> 6 == 0b10:
                 i += 2
             else:
                 return False
-        elif data[i] >> 4 == 0b1110:
+        elif byte >> 4 == 0b1110:
             if i + 2 < len(data) and data[i + 1] >> 6 == 0b10 and \
                     data[i + 2] >> 6 == 0b10:
                 i += 3
             else:
                 return False
-        elif data[i] >> 3 == 0b11110:
+        elif byte >> 3 == 0b11110:
             if i + 3 < len(data) and data[i + 1] >> 6 == 0b10 and \
                     data[i + 2] >> 6 == 0b10 and data[i + 3] >> 6 == 0b10:
                 i += 4
