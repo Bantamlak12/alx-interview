d5d88a578b3106073cd5991ded5ff50a80485277
Bantamlak <bantamlak29@gmail.com>
2024-02-27 19:00:58 +0300

    greedy approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,16 +11,29 @@ def makeChange(coins, total):
     - coins: A pile of coins of different values.
     - total: Total amount to meet.
     """
+    # Using Dynamic programming
+    # if total == 0 or total < 0:
+    #     return 0
+
+    # dp = [float('inf')] * (total + 1)
+    # dp[0] = 0
+
+    # for coin in coins[::-1]:
+    #     for amount in range(coin, total + 1):
+    #         dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    # if dp[-1] == float('inf'):
+    #     return -1
+    # return dp[-1]
+
+    # Using Greedy algorithm
     if total == 0 or total < 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    minCoins = 0
 
     for coin in coins[::-1]:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    if dp[-1] == float('inf'):
-        return -1
-    return dp[-1]
+        while total >= coin:
+            total -= coin
+            minCoins += 1
+    return minCoins if total == 0 else -1
